3eac2914e822bd3093543e30caa2c718527ebac4
Niladri <niladrisengit12@gmail.com>
2023-01-11 12:54:29 +0530

    initial commit

--- react-bricks/bricks/MyHeroUnit.tsx
+++ react-bricks/bricks/MyHeroUnit.tsx
@@ -17,46 +17,22 @@ interface HeroUnitProps {
 //=============================
 const MyHeroUnit: types.Brick<HeroUnitProps> = ({ padding }) => {
   return (
-    <div className={`max-w-xl mx-auto px-6 ${padding === 'big' ? 'py-20' : 'py-12'}`}>
-      <div>
+    <div className={`  max-w-xl mx-auto px-6 ${padding === 'big' ? 'py-20' : 'py-12'}`}>
+      <div className='flex' >
         <Image
           propName="icon"
           alt="Icon"
           maxWidth={80}
           aspectRatio={1}
+          imageClassName="w-20 mb-5 mx-2 "
+          className="px-3"
+        />
+         <Image
+          propName="icon"
+          alt="Icon"
+          maxWidth={80}
+          aspectRatio={1}
           imageClassName="w-20 mb-5 mx-auto"
-        />
-        <Text
-          renderBlock={(props) => (
-            <h1 className="text-3xl sm:text-4xl text-center font-black text-gray-900 dark:text-white leading-tight mb-3">
-              {props.children}
-            </h1>
-          )}
-          multiline={true}
-          softLineBreak={true}
-          placeholder="Type a title..."
-          propName="title"
-        />
-        <RichText
-          renderBlock={(props) => (
-            <p className="text-xl text-center leading-relaxed text-gray-700 dark:text-gray-100">
-              {props.children}
-            </p>
-          )}
-          placeholder="Type a text..."
-          propName="text"
-          allowedFeatures={[
-            types.RichTextFeatures.Bold,
-            types.RichTextFeatures.Italic,
-            types.RichTextFeatures.Highlight,
-            types.RichTextFeatures.Code,
-            types.RichTextFeatures.Link,
-          ]}
-          renderCode={(props) => (
-            <code className="text-sm py-1 px-2 bg-gray-200 dark:bg-gray-700 rounded">
-              {props.children}
-            </code>
-          )}
         />
       </div>
     </div>
